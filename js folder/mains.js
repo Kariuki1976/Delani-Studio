99ad21ea8d10654d05d79e9410710a6f24708886
Kariuki1976 <emmanuel.mwangi@student.moringaschool.com>
2022-03-14 14:04:34 -0400

    add form validation

--- js folder/mains.js
+++ js folder/mains.js
@@ -140,7 +140,7 @@ $(document).ready(function() {
         var email = $("input#email").val();
         var message = $("textarea#subject").val();
         if (name && email) {
-            alert(name + ", we have received your message. Thank you for considering us. We shall respond as soon as possible.Cheers!");
+            alert(name + ", we have received your message. Thank you for considering us. We shall respond as soon as possible.Big up!!");
         } else {
             alert("Please enter your name and email!");
         }
